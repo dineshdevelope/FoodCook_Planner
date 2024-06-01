30704c078247e75bb4a95f87bcd48c39924d70cb
dineshdevelope <dineshindevelope@gmail.com>
2024-06-01 23:49:08 +0530

    delete-function

--- src/js/main.js
+++ src/js/main.js
@@ -5,16 +5,18 @@ const itemInput = document.getElementById("itemInput");
 const addBtn = document.getElementById("addBtn");
 //console.log(addBtn);
 const listItem = document.getElementById("listItem");
-console.log(listItem);
+//console.log(listItem);
 const deleteBtn = document.getElementsByClassName("deleteBtn");
 //console.log(deleteBtn);
+const clearBtn = document.getElementById("clearBtn");
+//console.log(clearBtn);
 
 function addItem(e) {
   e.preventDefault();
   const inputValue = itemInput.value;
-  //console.log(inputValue);
 
   if (inputValue) {
+    clearBtn.style.display = "block";
     const createList = document.createElement("li");
     createList.className =
       "flex items-center justify-between bg-gray-600 p-2 rounded text-white max-w-sm mx-auto mt-2 mb-7 capitalize hover:bg-gray-700";
@@ -24,6 +26,7 @@ function addItem(e) {
     createBtn.className =
       "bg-red-600 w-10 p-2 rounded hover:bg-red-700 deleteBtn hover:font-extrabold";
     createBtn.innerText = "x";
+    createBtn.setAttribute("onClick", "removeItem(event)");
 
     createList.append(createBtn);
 
@@ -34,3 +37,12 @@ function addItem(e) {
 }
 
 formEl.addEventListener("submit", addItem);
+
+function clearFunction() {
+  listItem.innerHTML = "";
+  if (listItem.children.length === 0) {
+    clearBtn.style.display = "none";
+  }
+}
+
+clearBtn.addEventListener("click", clearFunction);
